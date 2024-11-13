e6a8545d91ba36381e7f715e88041157919dcc9c
hyobin-park <qlswkd03@gmail.com>
2024-11-13 14:10:20 +0900

    효빈 - 로그인, 회원가입, 인덱스 페이지 수정

--- bananas/src/main/java/com/nongshim/infra/baCommon/BaCommonController.java
+++ bananas/src/main/java/com/nongshim/infra/baCommon/BaCommonController.java
@@ -21,6 +21,74 @@ public class BaCommonController {
 	@Autowired
 	BaMemberService baMemberService;
 
+	// 사용자 메인화면
+	// INDEX
+	@RequestMapping(value = "/usr/v1/infra/common/baUsrIndex")
+	public String baUsrIndex() {
+		return "/usr/v1/infra/common/baUsrIndex";
+	}
+	
+	// 사용자 로그인
+	// SIGNIN
+	@RequestMapping(value = "/usr/v1/infra/common/baUsrSignIn")
+	public String baUsrSignIn() {
+		return "/usr/v1/infra/common/baUsrSignIn";
+	}
+	
+	// 사용자 로그인 세션유지
+	// LoginProc
+	@ResponseBody
+	@RequestMapping(value = "/usr/v1/infra/common/baUsrSignInProc")
+	public Map<String, Object> baUsrSignInProc(BaMemberDto baMemberDto, HttpSession httpSession) {
+
+		Map<String, Object> returnMap = new HashMap<String, Object>(); // 결과를 담기 위한 맵 생성
+
+		BaMemberDto rtUser = baMemberService.xdmSelectOneSignin(baMemberDto); // 사용자 정보 조회
+
+		if (rtUser != null) { // 객체를 대상으로 null을 검사
+
+			BaMemberDto rtUser2 = baMemberService.xdmSelectOneID(baMemberDto); // 로그인 후 세션 정보 저장
+
+			if (rtUser2 != null) {
+				// 세션값 저장
+				httpSession.setMaxInactiveInterval(60 * Constants.SESSION_MINUTE_XDM); // 60second * 30 = 30minute
+				httpSession.setAttribute("sessSeqXdm", rtUser2.getBameSeq());
+				httpSession.setAttribute("sessIdXdm", rtUser2.getBameId());
+				httpSession.setAttribute("sessNameXdm", rtUser2.getBameName());
+				httpSession.setAttribute("sessGradeXdm", rtUser2.getBameGrade());
+				// 성공 응답 설정
+				returnMap.put("rt", "success");
+				// 저장된 세션값 확인
+				System.out.println("sessSeqXdm: " + httpSession.getAttribute("sessSeqXdm"));
+				System.out.println("sessIdXdm: " + httpSession.getAttribute("sessIdXdm"));
+				System.out.println("sessNameXdm: " + httpSession.getAttribute("sessNameXdm"));
+				System.out.println("sessGradeXdm: " + httpSession.getAttribute("sessGradeXdm"));
+			} else {
+				returnMap.put("rt", "fail"); // 실패 응답 설정
+			}
+		}
+		return returnMap;
+	}
+	
+	// 사용자 로그아웃
+	// LogoutProc
+	@ResponseBody
+	@RequestMapping(value = "/usr/v1/infra/common/baUsrSignOutProc")
+	public Map<String, Object> baUsrSignOutProc(HttpSession httpSession) {
+		Map<String, Object> returnMap = new HashMap<String, Object>();
+		httpSession.invalidate();
+		returnMap.put("rt", "success");
+		return returnMap;
+	}
+	
+	// 사용자 회원가입
+	// SIGNUP
+	@RequestMapping(value = "/usr/v1/infra/common/baUsrSignUp")
+	public String baUsrSignUp() {
+		return "/usr/v1/infra/common/baUsrSignUp";
+	}
+	
+	
 	// 관리자 메인화면
 	// INDEX
 	@RequestMapping(value = "/xdm/v1/infra/common/baXdmIndex")
