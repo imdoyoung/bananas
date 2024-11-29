467ab1680ceea08fe0b4021e99a307c49d9f9bca
hyobin-park <qlswkd03@gmail.com>
2024-11-29 14:06:09 +0900

    혜원+효빈 - 공공데이터 api 수정 중

--- bananas/src/main/java/com/nongshim/infra/baPlace/BaPlaceController.java
+++ bananas/src/main/java/com/nongshim/infra/baPlace/BaPlaceController.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -16,46 +17,55 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class BaPlaceController {
 
 	@RequestMapping(value="/usr/v1/infra/baPlace/baPlaceList")
-	public String baPlaceList() throws Exception {
+	public String baPlaceList(Model model) throws Exception {
 		
-		// API
-		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551011/KorPetTourService/detailIntro?serviceKey=NbI1b9h7KgOqb%2B31Yf1PETThy2SUYDCddS95jD5cnhBizLTSpMYO%2F3DyipSM3V1VPaqOXAonaoV%2FeLouDlud4Q%3D%3D&contentId=1059479&contentTypeId=12&MobileOS=ETC&MobileApp=AppTest"); /*URL*/
-		urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=NbI1b9h7KgOqb%2B31Yf1PETThy2SUYDCddS95jD5cnhBizLTSpMYO%2F3DyipSM3V1VPaqOXAonaoV%2FeLouDlud4Q%3D%3D"); /*Service Key*/
-		urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
-		urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("9", "UTF-8")); /*한 페이지 결과 수*/
-		urlBuilder.append("&" + URLEncoder.encode("MobileOS","UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8")); /*OS 구분*/
-		urlBuilder.append("&" + URLEncoder.encode("MobileApp","UTF-8") + "=" + URLEncoder.encode("AppTest", "UTF-8")); /*서비스명(어플명)*/
-		urlBuilder.append("&" + URLEncoder.encode("contentId","UTF-8") + "=" + URLEncoder.encode("1059479", "UTF-8")); /*콘텐츠ID*/
-		urlBuilder.append("&" + URLEncoder.encode("contentTypeId","UTF-8") + "=" + URLEncoder.encode("12", "UTF-8")); /*관광타입*/
-		urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*응답데이터 형식(xml/json) default : xml*/
-
-		URL url = new URL(urlBuilder.toString());
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Content-type", "application/json");
-		System.out.println("Response code: " + conn.getResponseCode());
-		BufferedReader rd;
-		
-		if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-			rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		} else {
-			rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-		}
-		StringBuilder sb = new StringBuilder();
-		String line;
-		while ((line = rd.readLine()) != null) {
-			sb.append(line);
-		}
-		rd.close();
-		conn.disconnect();
-		System.out.println(sb.toString());
+		 // API
+		 // 1. URL을 만들기 위한 StringBuilder
+		 StringBuilder urlBuilder = new StringBuilder("https://www.pettravel.kr/api/detailSeqArea.do?areaCode=AC01&contentNum=2"); /*URL*/
+		 
+		 // 2. 오픈 API의 요청 규격에 맞는 파라미터 생성
+//	     urlBuilder.append("?" + URLEncoder.encode("page","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+//	     urlBuilder.append("&" + URLEncoder.encode("pageBlock","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+//	     urlBuilder.append("&" + URLEncoder.encode("areaCode","UTF-8") + "=" + URLEncoder.encode("AC01", "UTF-8")); /*지역 코드*/
+	     
+	     // 3. URL 객체 생성 (String으로 변환)
+	     URL url = new URL(urlBuilder.toString());
+	     // 4. 요청하고자 하는 URL과 통신하기 위한 Connection 객체 생성
+	     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	     // 5. 통신을 위한 메소드 세팅 ("GET")
+	     conn.setRequestMethod("GET");
+	     // 6. 통신을 위한 Content-type 세팅 ("JSON" 으로 해야함 - reuslt의 값은 String이기 때문에)
+	     conn.setRequestProperty("Content-type", "application/json");
+	     // 7. 통신 응답 코드 확인
+	     System.out.println("Response code: " + conn.getResponseCode());
+	     // 8. 전달받은 데이터를 BufferedReader 객체로 저장. 오류가 날 경우 error발생
+	     BufferedReader rd;
+	     if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+	    	 rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	     } else {
+	    	 rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+	     }
+	     
+	     // 9. 저장된 데이터를 라인별로 읽어 StringBuilder 객체로 저장
+	     StringBuilder sb = new StringBuilder();
+	     String line;
+	     while ((line = rd.readLine()) != null) {
+	    	 sb.append(line);
+	     }
+	     
+	     // 10. 객체 해제
+	     rd.close();
+	     conn.disconnect();
+	     System.out.println(sb.toString());
 				
 		ObjectMapper objectMapper = new ObjectMapper();
 		JsonNode node = objectMapper.readTree(sb.toString());
-			
-		System.out.println("node.get(\"header\").get(\"resultCode\").asText(): " + node.get("header").get("resultCode").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("header").get("resultMsg").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(0).get("KIT_PROD_QTY").asText());
+//		
+//		model.addAttribute("node", node.get("resultList").get("title"));
+//			
+//		System.out.println("node.get(\"header\").get(\"resultCode\").asText(): " + node.get("header").get("resultCode").asText());
+//		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("header").get("resultMsg").asText());
+//		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(0).get("KIT_PROD_QTY").asText());
 		
 		return "/usr/v1/infra/baPlace/baPlaceList";
 	}
