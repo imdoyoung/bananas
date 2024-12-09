43c232f5ffc3ed979eaa523b848359d18d40aec5
hyobin-park <qlswkd03@gmail.com>
2024-12-09 14:57:31 +0900

    시터 컨트롤러 수정 - 도영이 받아라아

--- bananas/src/main/java/com/nongshim/infra/baSitterList/BaUsrSitterController.java
+++ bananas/src/main/java/com/nongshim/infra/baSitterList/BaUsrSitterController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
 @Controller
 public class BaUsrSitterController {
 	@Autowired
