5a4ad0f2070100ccab01fb3b884197bedccd8f03
imdoyoung <bear2008@naver.com>
2024-12-14 23:21:13 +0900

    도영 : 채팅 수정

--- bananas/src/main/java/com/nongshim/infra/firebase/FirebaseConfig.java
+++ bananas/src/main/java/com/nongshim/infra/firebase/FirebaseConfig.java
@@ -1,4 +1,4 @@
-// Firebase Admin SDK를 초기화하는 구성 클래스 >> 통신준비 완료
+//// Firebase Admin SDK를 초기화하는 구성 클래스 >> 통신준비 완료
 package com.nongshim.infra.firebase;
 
 import java.io.FileInputStream;
@@ -16,31 +16,82 @@ import com.google.firebase.database.FirebaseDatabase;
 @Configuration
 public class FirebaseConfig {
 
-	@Bean
+    // Firebase 초기화
+    @Bean
     public FirebaseApp initializeFirebase() throws IOException {
-        
-		// FirebaseApp이 이미 초기화되었는지 확인
-		if (FirebaseApp.getApps().isEmpty()) {
+        if (FirebaseApp.getApps().isEmpty()) {
+            FileInputStream serviceAccount = new FileInputStream(new ClassPathResource("firebase/firebase-adminsdk.json").getFile());
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://superbad-bananas-firebase-chat-default-rtdb.firebaseio.com/")
+                    .build();
 
-//				FileInputStream serviceAccount = new FileInputStream("/bananas/src/main/resources/firebase/firebase-adminsdk.json");
-				FileInputStream serviceAccount = new FileInputStream(new ClassPathResource("firebase/firebase-adminsdk.json").getFile());
-				
-				FirebaseOptions options = FirebaseOptions.builder()
-						.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-						.setDatabaseUrl("https://superbad-bananas-firebase-chat-default-rtdb.firebaseio.com/")
-						.build();
-			
-				return FirebaseApp.initializeApp(options);
-		} else {
-			// 이미 초기화된 경우 기존 FirebaseApp 반환
-            return FirebaseApp.getInstance();
-		}
+            return FirebaseApp.initializeApp(options);
+        } else {
+            return FirebaseApp.getInstance(); // 이미 초기화된 FirebaseApp 반환
+        }
+    }
 
+    // FirebaseDatabase 인스턴스를 제공하는 Bean
+    @Bean
+    public FirebaseDatabase firebaseDatabase() throws IOException {
+        FirebaseApp firebaseApp = initializeFirebase(); // FirebaseApp 초기화
+        return FirebaseDatabase.getInstance(firebaseApp); // FirebaseApp을 명시적으로 전달
     }
-	
-	@Bean
-    public FirebaseDatabase firebaseDatabase() {
-        return FirebaseDatabase.getInstance();
-    }
-	
 }
+
+
+
+
+
+
+
+
+
+
+
+//package com.nongshim.infra.firebase;
+//
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import com.google.firebase.database.FirebaseDatabase;
+//
+//@Configuration
+//public class FirebaseConfig {
+//
+//	@Bean
+//    public FirebaseApp initializeFirebase() throws IOException {
+//        
+//		// FirebaseApp이 이미 초기화되었는지 확인
+//		if (FirebaseApp.getApps().isEmpty()) {
+//
+////				FileInputStream serviceAccount = new FileInputStream("/bananas/src/main/resources/firebase/firebase-adminsdk.json");
+//				FileInputStream serviceAccount = new FileInputStream(new ClassPathResource("firebase/firebase-adminsdk.json").getFile());
+//				
+//				FirebaseOptions options = FirebaseOptions.builder()
+//						.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//						.setDatabaseUrl("https://superbad-bananas-firebase-chat-default-rtdb.firebaseio.com/")
+//						.build();
+//			
+//				return FirebaseApp.initializeApp(options);
+//		} else {
+//			// 이미 초기화된 경우 기존 FirebaseApp 반환
+//            return FirebaseApp.getInstance();
+//		}
+//
+//    }
+//	
+//	@Bean
+//    public FirebaseDatabase firebaseDatabase() {
+//        return FirebaseDatabase.getInstance();
+//    }
+//	
+//}
--- bananas/src/main/resources/static/usr/v1/js/firebase/firebase.js
+++ bananas/src/main/resources/static/usr/v1/js/firebase/firebase.js
@@ -1,8 +1,8 @@
 // firebase SDK - CDN
 
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
