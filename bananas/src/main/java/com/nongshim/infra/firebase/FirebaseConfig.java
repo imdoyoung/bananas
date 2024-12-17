afb9580723665ef40a3d15c54be78470b5ba753e
imdoyoung <bear2008@naver.com>
2024-12-17 16:33:49 +0900

    도영 : firebase 채팅 이제 배포 됩니다~

--- bananas/src/main/java/com/nongshim/infra/firebase/FirebaseConfig.java
+++ bananas/src/main/java/com/nongshim/infra/firebase/FirebaseConfig.java
@@ -1,8 +1,9 @@
 //// Firebase Admin SDK를 초기화하는 구성 클래스 >> 통신준비 완료
+
 package com.nongshim.infra.firebase;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +21,9 @@ public class FirebaseConfig {
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
-            FileInputStream serviceAccount = new FileInputStream(new ClassPathResource("firebase/firebase-adminsdk.json").getFile());
+            // ClassPathResource를 통해 InputStream으로 리소스 로드
+            InputStream serviceAccount = new ClassPathResource("firebase/firebase-adminsdk.json").getInputStream();
+
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setDatabaseUrl("https://superbad-bananas-firebase-chat-default-rtdb.firebaseio.com/")
@@ -34,8 +37,7 @@ public class FirebaseConfig {
 
     // FirebaseDatabase 인스턴스를 제공하는 Bean
     @Bean
-    public FirebaseDatabase firebaseDatabase() throws IOException {
-        FirebaseApp firebaseApp = initializeFirebase(); // FirebaseApp 초기화
+    public FirebaseDatabase firebaseDatabase(FirebaseApp firebaseApp) {
         return FirebaseDatabase.getInstance(firebaseApp); // FirebaseApp을 명시적으로 전달
     }
 }
@@ -43,6 +45,49 @@ public class FirebaseConfig {
 
 
 
+
+
+
+//package com.nongshim.infra.firebase;
+//
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import com.google.firebase.database.FirebaseDatabase;
+//
+//@Configuration
+//public class FirebaseConfig {
+//
+//    // Firebase 초기화
+//    @Bean
+//    public FirebaseApp initializeFirebase() throws IOException {
+//        if (FirebaseApp.getApps().isEmpty()) {
+//            FileInputStream serviceAccount = new FileInputStream(new ClassPathResource("firebase/firebase-adminsdk.json").getFile());
+//            FirebaseOptions options = FirebaseOptions.builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .setDatabaseUrl("https://superbad-bananas-firebase-chat-default-rtdb.firebaseio.com/")
+//                    .build();
+//
+//            return FirebaseApp.initializeApp(options);
+//        } else {
+//            return FirebaseApp.getInstance(); // 이미 초기화된 FirebaseApp 반환
+//        }
+//    }
+//
+//    // FirebaseDatabase 인스턴스를 제공하는 Bean
+//    @Bean
+//    public FirebaseDatabase firebaseDatabase() throws IOException {
+//        FirebaseApp firebaseApp = initializeFirebase(); // FirebaseApp 초기화
+//        return FirebaseDatabase.getInstance(firebaseApp); // FirebaseApp을 명시적으로 전달
+//    }
+//}
 
 
 
